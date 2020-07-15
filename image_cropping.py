086b0f6f3d224ddc51e98873e245abefd7813a7d
YuichiNAGAO <51072685+YuichiNAGAO@users.noreply.github.com>
2020-07-15 12:24:34 +0200

    Update image_cropping.py

--- image_cropping.py
+++ image_cropping.py
@@ -6,6 +6,7 @@ import numpy as np
 import shutil
 from tqdm import tqdm
 import random
+import argparse
 
 
 
@@ -35,9 +36,6 @@ def save_list(obj,filename):
         f.write("\n")
     f.close()
     
-with open("image_list.txt") as f:
-    l_strip = [s.strip() for s in f.readlines()]    
-
 arange_dir("data/custom/images_with_bb")
 arange_dir("data/custom/images_with_bb_2")
 arange_dir("data/custom/labels_not_yolo")
@@ -45,175 +43,196 @@ arange_dir("data/custom/labels_not_yolo_2")
 arange_dir("data/custom/labels")
 arange_dir("data/custom/images")
 arange_dir("data/custom/labels_2")
-
+ 
 step=208
 size=832 
-num_file=0
-    
-for image_cnn in l_strip:
-    print("==========Processing {} now...===========".format(image_cnn.split()[0]) )
-    file_png="img_and_cnn/"+image_cnn.split()[0]
-    file_cnn="img_and_cnn/"+image_cnn.split()[2]
-    img = cv2.imread(file_png)
-    img_raw = cv2.imread(file_png)
-    name_file="data/"+image_cnn.split()[0].split('_')[0]+'_'+image_cnn.split()[0].split('_')[1]+"_table.txt"
-    with open(name_file) as f:
-        table = [s.strip().split() for s in f.readlines()[1:]]  
-    height=img.shape[0]
-    width=img.shape[1] 
-    nb_image_h=height//step
-    nb_image_w=width//step
-    number_im=(nb_image_h-3)*(nb_image_w-3)
-    position_cropped=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
-    position_cropped_yolo=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
-    position_cropped_2=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
-    position_cropped_yolo_2=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
-    for tree in table:
-        c1, c2 = (int(tree[2]), int(tree[4])), (int(tree[3]), int(tree[5]))
-        if tree[1]=="Raphia":
-            color=(255, 0, 0)
-        else:
-            color=(0, 0, 255)
-        cv2.rectangle(img, c1, c2, color)
-        t_size = cv2.getTextSize(tree[1], 0, fontScale=1 / 3, thickness=1)[0]
-        c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)
-        cv2.putText(img, tree[1], (c1[0], c1[1] + t_size[1]), 0, 1 / 3, [225, 255, 255], thickness=1, lineType=cv2.LINE_AA)        
-    cv2.imwrite(file_png.replace('.png','_remade.png'),img)
-    
-    print("Creating cropped table...")
-    for tree in tqdm(table):
-        x_center=(int(tree[2])+int(tree[3]))//2
-        y_center=(int(tree[4])+int(tree[5]))//2
-        if x_center>(nb_image_w*step) or y_center>(nb_image_h*step):
-            continue
-        x_cen=x_center//step
-        y_cen=y_center//step
-        x_ind=int(tree[3])//step-int(tree[2])//step
-        y_ind=int(tree[5])//step -int(tree[4])//step
+num_file=0    
+
+def operation(l_strip):
+    global step
+    global size
+    global num_file
+    for image_cnn in l_strip:
+        print("==========Processing {} now...===========".format(image_cnn.split()[0]) )
+        file_png="img_and_cnn/"+image_cnn.split()[0]
+        file_cnn="img_and_cnn/"+image_cnn.split()[2]
+        img = cv2.imread(file_png)
+        img_raw = cv2.imread(file_png)
+        name_file="data/"+image_cnn.split()[0].split('_')[0]+'_'+image_cnn.split()[0].split('_')[1]+"_table.txt"
+        with open(name_file) as f:
+            table = [s.strip().split() for s in f.readlines()[1:]]  
+        height=img.shape[0]
+        width=img.shape[1] 
+        nb_image_h=height//step
+        nb_image_w=width//step
+        number_im=(nb_image_h-3)*(nb_image_w-3)
+        position_cropped=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
+        position_cropped_yolo=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
+        position_cropped_2=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
+        position_cropped_yolo_2=[[[] for j in range(nb_image_w-3)] for i in range(nb_image_h-3)]
+        for tree in table:
+            c1, c2 = (int(tree[2]), int(tree[4])), (int(tree[3]), int(tree[5]))
+            if tree[1]=="Raphia":
+                color=(255, 0, 0)
+            else:
+                color=(0, 0, 255)
+            cv2.rectangle(img, c1, c2, color)
+            t_size = cv2.getTextSize(tree[1], 0, fontScale=1 / 3, thickness=1)[0]
+            c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
+            cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)
+            cv2.putText(img, tree[1], (c1[0], c1[1] + t_size[1]), 0, 1 / 3, [225, 255, 255], thickness=1, lineType=cv2.LINE_AA)        
+        cv2.imwrite(file_png.replace('.png','_remade.png'),img)
+
+        print("Creating cropped table...")
+        for tree in tqdm(table):
+            x_center=(int(tree[2])+int(tree[3]))//2
+            y_center=(int(tree[4])+int(tree[5]))//2
+            if x_center>(nb_image_w*step) or y_center>(nb_image_h*step):
+                continue
+            x_cen=x_center//step
+            y_cen=y_center//step
+            x_ind=int(tree[3])//step-int(tree[2])//step
+            y_ind=int(tree[5])//step -int(tree[4])//step
+
+            for j_x in range(4-x_ind):
+                for j_y in range(4-y_ind):
+                    y_pos=int(tree[4])//step-j_y
+                    x_pos=int(tree[2])//step-j_x
+                    if y_pos<0 or x_pos<0 or y_pos>=(nb_image_h-3) or x_pos>=(nb_image_w-3):
+                        continue 
+                    x_min=int(tree[2])-step*x_pos
+                    x_max=int(tree[3])-step*x_pos
+                    y_min=int(tree[4])-step*y_pos
+                    y_max=int(tree[5])-step*y_pos
+                    position_cropped[y_pos][x_pos].append([tree[1],x_min,x_max,y_min,y_max])     
+
+            for j_x in range(4):
+                for j_y in range(4):
+                    y_pos=y_cen-j_y
+                    x_pos=x_cen-j_x
+                    if y_pos<0 or x_pos<0 or y_pos>=(nb_image_h-3) or x_pos>=(nb_image_w-3):
+                        continue 
+                    x_min=int(tree[2])-step*x_pos
+                    x_max=int(tree[3])-step*x_pos
+                    y_min=int(tree[4])-step*y_pos
+                    y_max=int(tree[5])-step*y_pos
+                    if x_min<0:
+                        x_min=0
+                    if y_min<0:
+                        y_min=0
+                    if x_max>832:
+                        x_max=size
+                    if y_max>832:
+                        y_max=size
+                    position_cropped_2[y_pos][x_pos].append([tree[1],x_min,x_max,y_min,y_max]) 
+
+
+
+        print("Creating Yolo annotation...")  
+        for i,row in tqdm(enumerate(position_cropped)):
+            for j, col in enumerate(row):
+                number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
+                filename="data/custom/labels_not_yolo/image_"+number+".txt"
+                save_list(col,filename)
+                for obj in col:
+                    if obj[0]=="Coco":
+                        n_class=0
+                    elif  obj[0]=="Raphia":
+                        n_class=1
+                    else:
+                        n_class=2
+
+                    position_cropped_yolo[i][j].append([n_class,float((obj[1]+obj[2])/2/size),float((obj[3]+obj[4])/2/size),float((obj[2]-obj[1])/size),float((obj[4]-obj[3])/size)])
+
+        for i,row in tqdm(enumerate(position_cropped_2)):
+            for j, col in enumerate(row):
+                number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
+                filename="data/custom/labels_not_yolo_2/image_"+number+".txt"
+                save_list(col,filename)
+                for obj in col:
+                    if obj[0]=="Coco":
+                        n_class=0
+                    elif  obj[0]=="Raphia":
+                        n_class=1
+                    else:
+                        n_class=2
+                    position_cropped_yolo_2[i][j].append([n_class,float((obj[1]+obj[2])/2/size),float((obj[3]+obj[4])/2/size),float((obj[2]-obj[1])/size),float((obj[4]-obj[3])/size)])
+
+    #labels        
+        print("Saving Yolo annotation...")  
+        for i,row in tqdm(enumerate(position_cropped_yolo)):
+            for j,img_pos in enumerate(row):
+                number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
+                filename="data/custom/labels/image_"+number+".txt"
+
+                save_list(img_pos,filename)
+
+        for i,row in tqdm(enumerate(position_cropped_yolo_2)):
+            for j,img_pos in enumerate(row):
+                number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
+                filename="data/custom/labels_2/image_"+number+".txt"
+
+                save_list(img_pos,filename)
+    #images   
+        print("Saving images...")  
+        for y in tqdm(range(nb_image_h-3)):
+            for x in range(nb_image_w-3):
+                number=str(y*(nb_image_w-3)+x+num_file).zfill(5)
+                im1=np.copy(img_raw[y*step:y*step+size,x*step:x*step+size])
+                im2=np.copy(img_raw[y*step:y*step+size,x*step:x*step+size])
+                im3=np.copy(img_raw[y*step:y*step+size,x*step:x*step+size])
+                im1 = cv2.resize(im1 , (416, 416))
+                cv2.imwrite("data/custom/images/image_"+number+".png",im1)
+                for i in position_cropped[y][x]:
+                    c1, c2 = (i[1], i[3]), (i[2], i[4])
+                    if i[0]=="Raphia":
+                        color=(255, 0, 0)
+                    elif  i[0]=="Coco":
+                        color=(0, 0, 255)
+                    else:
+                         color=(36, 74, 247)
+
+                    cv2.rectangle(im2, c1, c2, color)
+                    t_size = cv2.getTextSize(i[0], 0, fontScale=1 / 3, thickness=1)[0]
+                    c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
+                    cv2.rectangle(im2, c1, c2, color, -1, cv2.LINE_AA)
+                    cv2.putText(im2, i[0], (c1[0], c1[1] + t_size[1]), 0, 1 / 3, [225, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+                cv2.imwrite("data/custom/images_with_bb/image_bb_"+number+".png",im2) 
+                for i in position_cropped_2[y][x]:
+                    c1, c2 = (i[1], i[3]), (i[2], i[4])
+                    if i[0]=="Raphia":
+                        color=(255, 0, 0)
+                    elif  i[0]=="Coco":
+                        color=(0, 0, 255)
+                    else:
+                         color=(36, 74, 247)
+
+
+                    cv2.rectangle(im3, c1, c2, color)
+                    t_size = cv2.getTextSize(i[0], 0, fontScale=1 / 3, thickness=1)[0]
+                    c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
+                    cv2.rectangle(im3, c1, c2, color, -1, cv2.LINE_AA)
+                    cv2.putText(im3, i[0], (c1[0], c1[1] + t_size[1]), 0, 1 / 3, [225, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+                cv2.imwrite("data/custom/images_with_bb_2/image_bb_"+number+".png",im3) 
+
+        num_file+=number_im
         
-        for j_x in range(4-x_ind):
-            for j_y in range(4-y_ind):
-                y_pos=int(tree[4])//step-j_y
-                x_pos=int(tree[2])//step-j_x
-                if y_pos<0 or x_pos<0 or y_pos>=(nb_image_h-3) or x_pos>=(nb_image_w-3):
-                    continue 
-                x_min=int(tree[2])-step*x_pos
-                x_max=int(tree[3])-step*x_pos
-                y_min=int(tree[4])-step*y_pos
-                y_max=int(tree[5])-step*y_pos
-                position_cropped[y_pos][x_pos].append([tree[1],x_min,x_max,y_min,y_max])     
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--shuffle', action='store_true')
+opt = parser.parse_args()
         
-        for j_x in range(4):
-            for j_y in range(4):
-                y_pos=y_cen-j_y
-                x_pos=x_cen-j_x
-                if y_pos<0 or x_pos<0 or y_pos>=(nb_image_h-3) or x_pos>=(nb_image_w-3):
-                    continue 
-                x_min=int(tree[2])-step*x_pos
-                x_max=int(tree[3])-step*x_pos
-                y_min=int(tree[4])-step*y_pos
-                y_max=int(tree[5])-step*y_pos
-                if x_min<0:
-                    x_min=0
-                if y_min<0:
-                    y_min=0
-                if x_max>832:
-                    x_max=size
-                if y_max>832:
-                    y_max=size
-                position_cropped_2[y_pos][x_pos].append([tree[1],x_min,x_max,y_min,y_max]) 
+with open("image_list_train.txt") as f:
+    l_strip = [s.strip() for s in f.readlines()]    
+operation(l_strip)
+
+nb_train=num_file
         
-                
-          
-    print("Creating Yolo annotation...")  
-    for i,row in tqdm(enumerate(position_cropped)):
-        for j, col in enumerate(row):
-            number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
-            filename="data/custom/labels_not_yolo/image_"+number+".txt"
-            save_list(col,filename)
-            for obj in col:
-                if obj[0]=="Coco":
-                    n_class=0
-                elif  obj[0]=="Raphia":
-                    n_class=1
-                else:
-                    n_class=2
-                    
-                position_cropped_yolo[i][j].append([n_class,float((obj[1]+obj[2])/2/size),float((obj[3]+obj[4])/2/size),float((obj[2]-obj[1])/size),float((obj[4]-obj[3])/size)])
-       
-    for i,row in tqdm(enumerate(position_cropped_2)):
-        for j, col in enumerate(row):
-            number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
-            filename="data/custom/labels_not_yolo_2/image_"+number+".txt"
-            save_list(col,filename)
-            for obj in col:
-                if obj[0]=="Coco":
-                    n_class=0
-                elif  obj[0]=="Raphia":
-                    n_class=1
-                else:
-                    n_class=2
-                position_cropped_yolo_2[i][j].append([n_class,float((obj[1]+obj[2])/2/size),float((obj[3]+obj[4])/2/size),float((obj[2]-obj[1])/size),float((obj[4]-obj[3])/size)])
-                
-#labels        
-    print("Saving Yolo annotation...")  
-    for i,row in tqdm(enumerate(position_cropped_yolo)):
-        for j,img_pos in enumerate(row):
-            number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
-            filename="data/custom/labels/image_"+number+".txt"
-
-            save_list(img_pos,filename)
-    
-    for i,row in tqdm(enumerate(position_cropped_yolo_2)):
-        for j,img_pos in enumerate(row):
-            number=str(i*(nb_image_w-3)+j+num_file).zfill(5)
-            filename="data/custom/labels_2/image_"+number+".txt"
-
-            save_list(img_pos,filename)
-#images   
-    print("Saving images...")  
-    for y in tqdm(range(nb_image_h-3)):
-        for x in range(nb_image_w-3):
-            number=str(y*(nb_image_w-3)+x+num_file).zfill(5)
-            im1=np.copy(img_raw[y*step:y*step+size,x*step:x*step+size])
-            im2=np.copy(img_raw[y*step:y*step+size,x*step:x*step+size])
-            im3=np.copy(img_raw[y*step:y*step+size,x*step:x*step+size])
-            im1 = cv2.resize(im1 , (416, 416))
-            cv2.imwrite("data/custom/images/image_"+number+".png",im1)
-            for i in position_cropped[y][x]:
-                c1, c2 = (i[1], i[3]), (i[2], i[4])
-                if i[0]=="Raphia":
-                    color=(255, 0, 0)
-                elif  i[0]=="Coco":
-                    color=(0, 0, 255)
-                else:
-                     color=(36, 74, 247)
-
-                cv2.rectangle(im2, c1, c2, color)
-                t_size = cv2.getTextSize(i[0], 0, fontScale=1 / 3, thickness=1)[0]
-                c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
-                cv2.rectangle(im2, c1, c2, color, -1, cv2.LINE_AA)
-                cv2.putText(im2, i[0], (c1[0], c1[1] + t_size[1]), 0, 1 / 3, [225, 255, 255], thickness=1, lineType=cv2.LINE_AA)
-            cv2.imwrite("data/custom/images_with_bb/image_bb_"+number+".png",im2) 
-            for i in position_cropped_2[y][x]:
-                c1, c2 = (i[1], i[3]), (i[2], i[4])
-                if i[0]=="Raphia":
-                    color=(255, 0, 0)
-                elif  i[0]=="Coco":
-                    color=(0, 0, 255)
-                else:
-                     color=(36, 74, 247)
-                  
-
-                cv2.rectangle(im3, c1, c2, color)
-                t_size = cv2.getTextSize(i[0], 0, fontScale=1 / 3, thickness=1)[0]
-                c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
-                cv2.rectangle(im3, c1, c2, color, -1, cv2.LINE_AA)
-                cv2.putText(im3, i[0], (c1[0], c1[1] + t_size[1]), 0, 1 / 3, [225, 255, 255], thickness=1, lineType=cv2.LINE_AA)
-            cv2.imwrite("data/custom/images_with_bb_2/image_bb_"+number+".png",im3) 
-            
-    num_file+=number_im
+with open("image_list_test.txt") as f:
+    l_strip = [s.strip() for s in f.readlines()]    
+operation(l_strip)
 
 path_train="data/custom/train.txt"
 path_valid="data/custom/valid.txt"
@@ -222,20 +241,39 @@ if os.path.exists(path_train):
 if os.path.exists(path_valid):
     os.remove(path_valid)
 
-files=glob.glob('data/custom/images/*')
+files=sorted(glob.glob('data/custom/images/*'))
 
-nb_train=int(len(files)*0.75)
 
-files_shuffled=random.sample(files, len(files))
+if opt.shuffle:
+    nb_train=int(len(files)*0.75)
 
-for file in files_shuffled[:nb_train]:
-    f = open(path_train, 'a') # 書き込みモードで開く
-    f.write(file+"\n") # 引数の文字列をファイルに書き込む
-    f.close()
+    files_shuffled=random.sample(files, len(files))
 
-for file in files_shuffled[nb_train:]:
-    f = open(path_valid, 'a') # 書き込みモードで開く
-    f.write(file+"\n") # 引数の文字列をファイルに書き込む
-    f.close()
+    for file in files_shuffled[:nb_train]:
+        f = open(path_train, 'a') # 書き込みモードで開く
+        f.write(file+"\n") # 引数の文字列をファイルに書き込む
+        f.close()
+
+    for file in files_shuffled[nb_train:]:
+        f = open(path_valid, 'a') # 書き込みモードで開く
+        f.write(file+"\n") # 引数の文字列をファイルに書き込む
+        f.close()
+
+else:
+    
+    for file in files[:nb_train]:
+        f = open(path_train, 'a') # 書き込みモードで開く
+        f.write(file+"\n") # 引数の文字列をファイルに書き込む
+        f.close()
+    if len(files)>nb_train*1.3:
+        for file in files[nb_train:int(nb_train*1.3)]:
+            f = open(path_valid, 'a') # 書き込みモードで開く
+            f.write(file+"\n") # 引数の文字列をファイルに書き込む
+            f.close()
+    else:
+        for file in files[nb_train:]:
+            f = open(path_valid, 'a') # 書き込みモードで開く
+            f.write(file+"\n") # 引数の文字列をファイルに書き込む
+            f.close()
     
     
